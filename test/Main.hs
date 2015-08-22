c70326fec7604ee5252f1da2499119897f9ba97d
agomezl <alegomez544@gmail.com>
2015-08-22 13:32:46 -0500

    new tests added

--- test/Main.hs
+++ test/Main.hs
@@ -12,9 +12,10 @@
 module Main where
 
 import SimAFA                     (accepts)
-import Data.Automata.AFA.Examples (StateM(..), SymbolM(..),m1,m2,m3)
-import Test.QuickCheck            (Arbitrary,arbitrary,elements)
+import Data.Automata.AFA.Examples (StateM(..), SymbolM(..),m1,m2,m3,m4,m5)
+import Test.QuickCheck            (Arbitrary,arbitrary,elements,forAll)
 import Test.QuickCheck            (maxSuccess,quickCheckWith,stdArgs)
+import Test.QuickCheck            (choose,listOf,Property)
 
 instance Arbitrary SymbolM where
     arbitrary = elements [A,B,C,D]
@@ -22,9 +23,12 @@ instance Arbitrary SymbolM where
 
 main ∷ IO ()
 main = do
-  printTest prop1 "m1"
-  printTest prop2 "m2"
-  printTest prop3 "m3"
+  let conf = stdArgs {maxSuccess = 1000}
+  printTest (quickCheckWith conf prop1) "m1"
+  printTest (quickCheckWith conf prop2) "m2"
+  printTest (quickCheckWith conf prop3) "m3"
+  printTest (quickCheckWith conf prop4) "m4"
+  printTest (quickCheckWith conf prop5) "m5"
 
 m1Check ∷ [SymbolM] → Bool
 m1Check s = finishAB && noC && noD
@@ -38,6 +42,33 @@ m2Check _ = False
 m3Check ∷ [SymbolM] → Bool
 m3Check = null
 
+m4Check ∷ [Int] → Bool
+m4Check l = s0 l
+    where
+      s0 (0:x) = s0 x || s1 x
+      s0 _     = False
+      s1 (1:x) = s1 x || s2 x
+      s1 _     = False
+      s2 (2:x) = s2 x || s3 x
+      s2 _     = False
+      s3 []    = True
+      s3 _     = False
+
+m5Check ∷ [Int] → Bool
+m5Check l = s0 l
+    where
+      s0 (0:x) = s0 x
+      s0 (1:x) = s1 x && s2 x
+      s0 _     = False
+      s1 []    = True
+      s1 (1:x) = s1 x && s2 x
+      s1 _     = False
+      s2 []    = True
+      s2 (1:x) = s1 x && s2 x
+      s2 _     = False
+
+
+
 prop1 ∷ [SymbolM] → Bool
 prop1 x = m1Check x == accepts m1 x
 
@@ -47,16 +78,23 @@ prop2 x = m2Check x == accepts m2 x
 prop3 ∷ [SymbolM] → Bool
 prop3 x = m3Check x == accepts m3 x
 
-printTest ∷ ([SymbolM] → Bool) → String → IO ()
-printTest prop msg = do
+prop4 ∷ Property
+prop4 = forAll (listOf $ choose (0,2)) rawProp
+    where rawProp x = m4Check x == accepts m4 x
+
+prop5 ∷ Property
+prop5 = forAll (listOf $ choose (0,1)) rawProp
+    where rawProp x = m5Check x == accepts m5 x
+
+printTest  ∷ IO () → String → IO ()
+printTest run msg = do
   putStrLn   "<div class=\"large-12 columns\">"
   putStrLn $ "<h4> Test ( " ++ msg ++ " )</h4>"
   putStrLn   "</div>"
   putStrLn   "<div class=\"large-12 columns\">"
   putStrLn   "<div class=\"panel\">"
   putStrLn   "<pre>"
-  let conf = stdArgs {maxSuccess = 1000}
-  quickCheckWith conf prop
+  run
   putStrLn   "</pre>"
   putStrLn   "</div>"
   putStrLn   "</div>"
